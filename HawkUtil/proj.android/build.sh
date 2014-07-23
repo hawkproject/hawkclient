36652a01ee249dd0450c7a57cdffbd5b731b23a6
四叶草 <380269273@qq.com>
2014-07-23 19:12:56 +0800

    清理版本

--- HawkUtil/proj.android/build.sh
+++
@@ -1,5 +0,0 @@
-ndk-build -B
-
-cp -v ../*.h jni/prebuild/include/
-cp -v obj/local/armeabi/libhawkutil.a jni/prebuild/libs/armeabi/
-cp -v obj/local/armeabi-v7a/libhawkutil.a jni/prebuild/libs/armeabi-v7a/
