705ff9a52926e2380346681daeef88c0ec928c12
四叶草 <380269273@qq.com>
2014-07-23 19:11:05 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	HawkUtil/HawkBinCfgFile.cpp
    	HawkUtil/HawkFile.cpp
    	HawkUtil/HawkFile.h
    	HawkUtil/HawkIPAddr.cpp
    	HawkUtil/HawkMemoryFile.cpp
    	HawkUtil/HawkMemoryFile.h
    	HawkUtil/HawkMsgListener.cpp
    	HawkUtil/HawkMsgListener.h
    	HawkUtil/HawkMsgManager.cpp
    	HawkUtil/HawkMsgManager.h
    	HawkUtil/HawkMsgPump.cpp
    	HawkUtil/HawkMsgPump.h
    	HawkUtil/HawkOSOperator.cpp
    	HawkUtil/HawkProtocol.cpp
    	HawkUtil/HawkProtocol.h
    	HawkUtil/HawkProtocolManager.cpp
    	HawkUtil/HawkSocketAddr.cpp
    	HawkUtil/HawkStdHeader.h
    	HawkUtil/HawkStringUtil.h
    	HawkUtil/HawkSysProtocol.h
    	HawkUtil/HawkUtil.h
    	HawkUtil/proj.android/build.sh
    	HawkUtil/proj.android/jni/Android.mk
    	HawkUtil/proj.win32/HawkUtil.vcxproj
    	HawkUtil/proj.win32/HawkUtil.vcxproj.filters

--- HawkUtil/proj.android/jni/prebuild/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := hawkutil_static
-LOCAL_MODULE_FILENAME := libhawkutil
-LOCAL_SRC_FILES := ./libs/$(TARGET_ARCH_ABI)/libhawkutil.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-include $(PREBUILT_STATIC_LIBRARY)
--- HawkUtil/proj.win32/build.bat
+++
@@ -1,2 +0,0 @@
-devenv /rebuild "Debug|Win32" HawkUtil.sln
-devenv /rebuild "Release|Win32" HawkUtil.sln
